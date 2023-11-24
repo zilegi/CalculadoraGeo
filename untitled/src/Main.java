00ed5bc2c77650f38ecfc9c3a614d30944de8eb3
gi_affonso@icloud.com <gi_affonso@icloud.com>
2023-11-24 16:25:46 -0300

    Alteracao da string opcao

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double media;
         int faltas ;
         boolean comportamento;
-        String opcao="Bosch";
+        String opcao="marcelo";
 
         while (!opcao.equalsIgnoreCase("sair")) {
             try {
