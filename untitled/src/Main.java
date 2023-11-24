3be33fca59b1d8e8f8dd4700f13bb1231211bfef
gi_affonso@icloud.com <gi_affonso@icloud.com>
2023-11-24 14:59:21 -0300

    Alterar nome do scanner

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         double media;
         int faltas ;
         boolean comportamento;
